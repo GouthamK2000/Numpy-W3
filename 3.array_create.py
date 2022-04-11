8742209beb182b1a46f245fd810f13797494c421
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-04-11 20:38:21 +0530

    step to create an array of n-dimensions

---
+++ 3.array_create.py
@@ -0,0 +1,25 @@
+import numpy as np
+
+arr=np.array(6)
+print(arr)   #0D array
+
+
+
+import numpy as np
+
+arr=np.array([1,7,3,2])
+print(arr)  #2D array
+
+
+
+import numpy as np
+
+arr=np.array([[1,2,3],[3,2,33]])
+print(arr)  #2D array
+
+import numpy as np
+
+arr=np.array([[[1,2,3],[2,34,65]],[[2,3,4],[5,3,4]]])
+print(arr) #3d array
+
+#number of  square brackets, after the curved brackets tell us the dimension of the array
