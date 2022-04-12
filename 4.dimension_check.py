2239d85fb1fed390927bc74bb5125968514fd4dd
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-04-12 21:56:44 +0530

    Ndim function helps us to check the dimension of the array

---
+++ 4.dimension_check.py
@@ -0,0 +1,11 @@
+import numpy as np
+
+x=np.array(1)
+y=np.array([1,2,4])
+z=np.array([[3,2,1],[879,76,4]])
+w=np.array([[[1,2,3],[6,5,7]],[[0,6,7],[3,4,2]]])
+
+print(x.ndim)
+print(y.ndim)
+print(z.ndim)
+print(w.ndim)  #ndim  tells us the dimensions of the specified array
