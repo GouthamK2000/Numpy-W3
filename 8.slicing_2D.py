2944023c6417e5f88a6305fd3d5bba59590175bf
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-04-16 22:11:26 +0530

    slicing in a 2D array

---
+++ 8.slicing_2D.py
@@ -0,0 +1,11 @@
+import numpy as np
+arr=np.array([[1,3,2,4,6,7],[21,3,4,1,1,31]])
+print(arr([1,1:4]))   # format [select array(s):element(s) to be  printed] 
+
+import numpy as np
+arr=np.array([[1,3,2,4,6,7],[21,3,4,1,1,31]])
+print(0:2,4) #returns the 5th(4+1th) element from both the arrays  as a single array
+
+import numpy as np
+arr=np.array([[1,3,2,4,6,7],[21,3,4,1,1,31]])
+print(0:2,2:5) #returns both set of elements form both the arrays.
