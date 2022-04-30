e458f56b61b18f230d8a718e3f793378f3c9f3d9
Goutham_Kumar <gouthamkumar2000@gmail.com>
2022-04-30 11:30:09 +0530

    concatenate is used to join 2 arrays

--- 21.concatenate.py
+++ 21.concatenate.py
@@ -17,3 +17,14 @@ arr=np.concatenate((arr1,arr2))
 print(arr)
 
 #arrays must be of the same dimensions, and the number of elements must be of the same size
+
+
+#case-3
+
+import numpy as np
+
+arr1=np.array([[3,3,5],[2,5,6]])
+arr2=np.array([[1,2,4],[0,2,9]])
+arr=np.concatenate((arr1,arr2),axis=1)  # Here all the elements in first array (of both the arrays)are put into one array, similarly to the second.
+print(arr)
+
